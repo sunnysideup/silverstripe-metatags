5cb78377cf236908137c27c6f0e46aed39917d31
nicolaas <github@sunnysideup.co.nz>
2009-03-23 20:33:58 +0000

    update _config.php

--- _config.php
+++ _config.php
@@ -1,16 +1,8 @@
 <?php
 /**
  * developed by www.sunnysideup.co.nz
- * author: Nicolaas modules [at] sunnysideup.co.nz*
+ * author: Nicolaas modules [at] sunnysideup.co.nz
  *
- * This Module allows you to add metatags easily...
- * add the following to your Page.ss file:
- * <head>
- *   <% base_tag %>
- *   $MetaTagsSunnySideUp
- * </head>
- * in page_controller->init()
- * add: $this->addRequirements() - this adds all the basic JS and CSS
  *
  **/
 
