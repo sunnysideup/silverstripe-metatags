219d7948b2afe04ac1321ea492f9db9bd0182796
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-10-09 07:19:53 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/metatags/trunk@5344 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/extension/MetaTagFileExtension.php
+++ code/extension/MetaTagFileExtension.php
@@ -11,81 +11,90 @@ class MetaTagFileExtension extends DataExtension {
 		if($this->stillNeedsToReplace && $this->owner instanceOf Image) {
 			$this->stillNeedsToReplace = false;
 			$oldObject = File::get()->byID($this->owner->ID);
-			$oldFileName = $oldObject->Name;
-			$oldPath = $oldObject->Filename;
-			if(!$oldPath) {
-				$oldPath = $oldObject->getFilename();
+			$oldFileName = "";
+			if($oldObject->Name) {
+				$oldFileName = $oldObject->Name;
 			}
-			$oldPath = str_replace($oldFileName, "", $oldPath);
-			$newFileName = $this->owner->Name;
-			$newPath = $this->owner->getRelativePath();
-			$newPath = str_replace($newFileName, "", $newPath);
-			if(($oldFileName != $newFileName) || ($oldPath != $oldPath)) {
-				$checks = MetaTagCMSControlFileUse::get()
-					->filter(
-						array(
-							"ConnectionType" => "DB",
-							"IsLiveVersion" => "0"
-						)
-					);
-				$siteTreeItemsToChange = SiteTree::get()
-					->where("\"SiteTree_ImageTracking\".\"FileID\" = ".$this->owner->ID)
-					->innerJoin("SiteTree_ImageTracking", "\"SiteTree_ImageTracking\".\"SiteTreeID\" = \"SiteTree\".\"ID\"");
-				if($siteTreeItemsToChange && $siteTreeItemsToChange->count()) {
-					foreach($siteTreeItemsToChange as $siteTreeItemToChange) {
-						$trackings = $siteTreeItemToChange->ImageTracking();
-						if($trackings && $trackings->count()) {
-							foreach($trackings as $tracking) {
-								$array = array(
-									"DataObjectClassName" => "SiteTree",
-									"DataObjectFieldName" => $tracking->FieldName,
-									"FileID" => $tracking->ID,
-									"TrackedID" => $siteTreeItemToChange->ID,
-									"IsImageTracking" => true
-								);
-								$item = new ArrayData($array);
-								$this->imageTrackingAddAgain[] = $array;
-								$checks->push($item);
+				if($oldFileName) {
+				$oldPath = "";
+				if(isset($oldObject->Filename)) {
+					$oldPath = $oldObject->Filename;
+				}
+
+				if(!$oldPath) {
+					$oldPath = $oldObject->getFilename();
+				}
+				$oldPath = str_replace($oldFileName, "", $oldPath);
+				$newFileName = $this->owner->Name;
+				$newPath = $this->owner->getRelativePath();
+				$newPath = str_replace($newFileName, "", $newPath);
+				if(($oldFileName != $newFileName) || ($oldPath != $oldPath)) {
+					$checks = MetaTagCMSControlFileUse::get()
+						->filter(
+							array(
+								"ConnectionType" => "DB",
+								"IsLiveVersion" => "0"
+							)
+						);
+					$siteTreeItemsToChange = SiteTree::get()
+						->where("\"SiteTree_ImageTracking\".\"FileID\" = ".$this->owner->ID)
+						->innerJoin("SiteTree_ImageTracking", "\"SiteTree_ImageTracking\".\"SiteTreeID\" = \"SiteTree\".\"ID\"");
+					if($siteTreeItemsToChange && $siteTreeItemsToChange->count()) {
+						foreach($siteTreeItemsToChange as $siteTreeItemToChange) {
+							$trackings = $siteTreeItemToChange->ImageTracking();
+							if($trackings && $trackings->count()) {
+								foreach($trackings as $tracking) {
+									$array = array(
+										"DataObjectClassName" => "SiteTree",
+										"DataObjectFieldName" => $tracking->FieldName,
+										"FileID" => $tracking->ID,
+										"TrackedID" => $siteTreeItemToChange->ID,
+										"IsImageTracking" => true
+									);
+									$item = new ArrayData($array);
+									$this->imageTrackingAddAgain[] = $array;
+									$checks->push($item);
+								}
 							}
 						}
 					}
-				}
-				if($checks && $checks->count()) {
-					foreach($checks as $check) {
-						$className = $check->DataObjectClassName;
-						$fieldName = $check->DataObjectFieldName;
-						if(isset($check->IsImageTracking) && $check->IsImageTracking) {
-							$dosToChange = $className::get()->filter(array("ID" => $check->TrackedID));
-						}
-						else {
-							$dosToChange = $className::get()->where("LOCATE('$oldFileName', \"".$fieldName."\") > 0");
-						}
-						if($dosToChange && $dosToChange->count()) {
-							foreach($dosToChange as $doToChange) {
-								$dom = new DOMDocument;
-								$dom->loadHTML('<?xml encoding="UTF-8">'.$doToChange->$fieldName);
-								foreach ($dom->getElementsByTagName('img') as $node) {
-									$oldSrc = $node->getAttribute('src' );
-									$newSrc = preg_replace('/'.str_replace("/", "\/", $oldPath).'(.*?)'.$oldFileName.'/', ''.$newPath.'$1'.$newFileName, $oldSrc);
-									if($oldSrc != $newSrc) {
-										$oldFilePath = Director::baseFolder()."/".$oldSrc;
-										$newFilePath = Director::baseFolder()."/".$newSrc;
-										if(file_exists($oldFilePath) && !file_exists($newFilePath)) {
-											rename($oldFilePath, $newFilePath);
+					if($checks && $checks->count()) {
+						foreach($checks as $check) {
+							$className = $check->DataObjectClassName;
+							$fieldName = $check->DataObjectFieldName;
+							if(isset($check->IsImageTracking) && $check->IsImageTracking) {
+								$dosToChange = $className::get()->filter(array("ID" => $check->TrackedID));
+							}
+							else {
+								$dosToChange = $className::get()->where("LOCATE('$oldFileName', \"".$fieldName."\") > 0");
+							}
+							if($dosToChange && $dosToChange->count()) {
+								foreach($dosToChange as $doToChange) {
+									$dom = new DOMDocument;
+									$dom->loadHTML('<?xml encoding="UTF-8">'.$doToChange->$fieldName);
+									foreach ($dom->getElementsByTagName('img') as $node) {
+										$oldSrc = $node->getAttribute('src' );
+										$newSrc = preg_replace('/'.str_replace("/", "\/", $oldPath).'(.*?)'.$oldFileName.'/', ''.$newPath.'$1'.$newFileName, $oldSrc);
+										if($oldSrc != $newSrc) {
+											$oldFilePath = Director::baseFolder()."/".$oldSrc;
+											$newFilePath = Director::baseFolder()."/".$newSrc;
+											if(file_exists($oldFilePath) && !file_exists($newFilePath)) {
+												rename($oldFilePath, $newFilePath);
+											}
+											$node->setAttribute('src', $newSrc);
 										}
-										$node->setAttribute('src', $newSrc);
 									}
-								}
-								$doToChange->encoding = 'UTF-8';
-								$doToChange->$fieldName = $dom->saveHTML();
-								$data = preg_replace('/'.str_replace("/", "\/", $oldPath).'(.*?)'.$oldFileName.'/', ''.$newPath.'$1'.$newFileName, $doToChange->$fieldName);
-								//$this->owner->generateFormattedImage($format, $arg1, $arg2)
-								if($doToChange instanceOf SiteTree) {
-									$doToChange->writeToStage('Stage');
-									$doToChange->publish('Stage', 'Live');
-								}
-								else {
-									$doToChange->write();
+									$doToChange->encoding = 'UTF-8';
+									$doToChange->$fieldName = $dom->saveHTML();
+									$data = preg_replace('/'.str_replace("/", "\/", $oldPath).'(.*?)'.$oldFileName.'/', ''.$newPath.'$1'.$newFileName, $doToChange->$fieldName);
+									//$this->owner->generateFormattedImage($format, $arg1, $arg2)
+									if($doToChange instanceOf SiteTree) {
+										$doToChange->writeToStage('Stage');
+										$doToChange->publish('Stage', 'Live');
+									}
+									else {
+										$doToChange->write();
+									}
 								}
 							}
 						}
