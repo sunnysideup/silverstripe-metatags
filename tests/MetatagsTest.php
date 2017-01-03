5cfc55b1818e60df232d73e0fe1262b20115542b
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:13:02 +1300

    PATCH: module clean-up

--- tests/MetatagsTest.php
+++ tests/MetatagsTest.php
@@ -1,12 +1,13 @@
 <?php
-class MetatagsTest extends SapphireTest
-{
-    protected $usesDatabase = false;
+class MetatagsTest extends SapphireTest {
 
-    protected $requiredExtensions = array();
+	protected $usesDatabase = false;
 
-    public function testMyMethod()
-    {
-        $this->assertEquals(1, 1);
-    }
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
 }
+
